d061355fe4959ea182e54aa269cf26d3d843d66e
Shanul Haque <sh2553@srmist.edu.in>
2026-03-25 13:38:05 +0530

    Add files via upload

---
+++ UC6.java
@@ -0,0 +1,26 @@
+public class UC6 {
+
+    public static void main(String[] args) {
+
+        String greeting;
+
+        if (args.length == 0) {
+            greeting = "Hello, World!";
+        } else {
+            StringBuilder nameBuilder = new StringBuilder();
+
+            for (String name : args) {
+                nameBuilder.append(name).append(", ");
+            }
+
+            if (nameBuilder.length() > 0) {
+                String name = nameBuilder.substring(0, nameBuilder.length() - 2);
+                greeting = "Hello, " + name + "!";
+            } else {
+                greeting = "Hello, World!";
+            }
+        }
+
+        System.out.println(greeting);
+    }
+}
